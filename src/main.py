86db3c8586faaa356345989ad3a772cd036619b4
inflame-ue <inflamokil@gmail.com>
2026-03-17 11:16:26 +0200

    implement TextNode class

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("This is a testing node", TextType.PLAIN_TEXT)
+    print(text_node)
+
+
+if __name__ == '__main__':
+    main()
