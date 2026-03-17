96ef358ad54b3d0f0440effc1be798de04a4e934
Ilia Oberemok <i.oberemok777@gmail.com>
2026-03-17 13:15:32 +0200

    implement text_node_to_html_node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from textnode import TextNode, TextType
+
 class HTMLNode:
     def __init__(
         self,
@@ -69,5 +71,21 @@ class ParentNode(HTMLNode):
         return f"<{self.tag}{parent_props}>{''.join(html_output)}</{self.tag}>"
 
 
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case TextType.PLAIN_TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINK_TEXT:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case TextType.IMAGE_TEXT:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("invalid TextType enum type")
     
 
