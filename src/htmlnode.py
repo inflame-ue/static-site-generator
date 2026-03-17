1644836ceb1450ba81d7f8ddbda8a21c25aad385
Ilia Oberemok <i.oberemok777@gmail.com>
2026-03-17 13:06:32 +0200

    implement the parent node and some basic tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,39 +1,73 @@
 class HTMLNode:
-
-    def __init__(self, tag: str = None, value: str = None, children: list['HTMLNode'] = None, props: dict[str, str] = None):
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: list["HTMLNode"] | None = None,
+        props: dict[str, str] | None = None,
+    ):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
-    
+
     def to_html(self):
         raise NotImplementedError()
 
     def props_to_html(self):
-        return " " + " ".join(f'{attribute}="{value}"' for attribute, value in self.props.items()) if self.props else ""
+        return (
+            " "
+            + " ".join(
+                f'{attribute}="{value}"' for attribute, value in self.props.items()
+            )
+            if self.props
+            else ""
+        )
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
-
-    def __init__(self, tag: str, value: str, props: dict[str, str] = None):
+    def __init__(self, tag: str, value: str, props: dict[str, str] | None = None):
         super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
         if not self.value:
             raise ValueError("All leaf nodes must have a value.")
-        
+
         if not self.tag:
             return self.value
 
         props_string = self.props_to_html()
-        return f"<{self.tag}{props_string if props_string else ''}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{props_string}>{self.value}</{self.tag}>"
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
-    
-    
 
+
+class ParentNode(HTMLNode):
+    
+    def __init__(
+        self, tag: str, children: list[HTMLNode], props: dict[str, str] | None = None
+    ):
+        super().__init__(tag=tag, children=children, props=props)
+    
+    def to_html(self):
+        
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+        
+        html_output = []
+        for child in self.children:
+            html_output.append(child.to_html())
+        
+        parent_props = self.props_to_html()
+        return f"<{self.tag}{parent_props}>{''.join(html_output)}</{self.tag}>"
+
+
+    
 
