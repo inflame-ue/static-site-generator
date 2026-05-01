c75c968efbedec95ddb6721c4922af692a2fc5af
Ilia Oberemok <i.oberemok777@gmail.com>
2026-05-01 16:48:27 +0300

    feat: implement generate_page that writes markdown converted to html into the content file

--- src/page.py
+++ src/page.py
@@ -1,3 +1,26 @@
+import os
+
 from blocks import markdown_to_html_node
 from markdown import extract_title
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r", encoding="utf-8") as markdown_file:
+        markdown = markdown_file.read()
+
+    with open(template_path, "r", encoding="utf-8") as template_file:
+        template = template_file.read()
+
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    output = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+    directory = os.path.dirname(dest_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+
+    with open(dest_path, "w", encoding="utf-8") as content_file:
+        content_file.write(output)
 
