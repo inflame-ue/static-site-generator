a063816d087f6f3f9e80f7424d9aba48de4716a1
Ilia Oberemok <i.oberemok777@gmail.com>
2026-05-01 17:24:40 +0300

    feat: account for github pages serving the side on a subpath with basepath

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import shutil
+import sys
 
 from page import generate_page
 
@@ -22,19 +23,25 @@ def copy_dir(source: str, destination: str) -> None:
 
 
 def generate_pages(
-    dir_path_content: str, template_path: str, dir_path_public: str
+    dir_path_content: str, template_path: str, dir_path_public: str, basepath: str
 ) -> None:
     for filename in os.listdir(dir_path_content):
         file_path = os.path.join(dir_path_content, filename)
         dest_file_path = os.path.join(dir_path_public, filename)
         if os.path.isfile(file_path) and os.path.split(file_path)[1].endswith(".md"):
             dest_file_path = pathlib.Path(dest_file_path).with_suffix(".html")
-            generate_page(file_path, template_path, dest_file_path) # type: ignore
+            generate_page(file_path, template_path, dest_file_path, basepath)  # type: ignore
         else:
-            generate_pages(file_path, template_path, dest_file_path)
+            generate_pages(file_path, template_path, dest_file_path, basepath)
 
 
 def main():
+    basepath = "/"
+    if len(sys.argv) > 2:
+        basepath = sys.argv[1]
+
+    
+
     copy_dir("static", "public")
     generate_pages("content/", "template.html", "public/")
 
--- src/page.py
+++ src/page.py
@@ -4,7 +4,7 @@ from blocks import markdown_to_html_node
 from markdown import extract_title
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r", encoding="utf-8") as markdown_file:
@@ -16,6 +16,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
     html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     output = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    output = output.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     directory = os.path.dirname(dest_path)
     if not os.path.exists(directory):
